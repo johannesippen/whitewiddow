de1771c5306792a14a3a7569073e885bed978fcc
Johannes Ippen <ich@johannesippen.com>
2013-10-25 00:46:39 +0200

    Add Permissions Flow

---
+++ public/js/permissions.js
@@ -0,0 +1,24 @@
+/* Specific Action */
+$('#welcome button').live('click',function(){
+  hasAccessToContacts();
+});
+$('#allowLocation button').live('click',function(){
+  allowCoreLocation();
+});
+$('#allowNotification button').live('click',function(){
+  // TODO: Placeholder
+  confirm('Whitewidow Would Like to Send You Push Notifications')
+});
+$('#addFriends button').live('click',function(){
+  $('body').attr('data-mode','friends')
+});
+/* Next Slide */
+$('.permission_action button').live('click',function(){
+  $(this).parents('.permission').remove();
+});
+/* Sort Permissions */
+$(function(){
+  $('#permissions .permission').each(function(){
+    $(this).prependTo('#permissions');
+  });
+});
