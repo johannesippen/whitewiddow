ea0bf577825513bc2f972939a6b2fad0d54f1c82
Johannes Ippen <jo@presskind.de>
2013-12-15 15:19:06 +0100

    Simplified Venue finding

--- public/js/location.js
+++ public/js/location.js
@@ -33,53 +33,35 @@ var staticMapUrl = function(my_ll, friend_ll, w, h, venue_ll) {
 
 // Get the venue from Foursquare and puts it into the Map
 var getVenue = function(location_ll, is_midpoint, is_my_hood, is_friend_hood, radius) {
-  if(activate_daytime) {
-    var category = activity[getDaytime()]; // Foursquare Nightlife
-  } else {
-    var category = '4bf58dd8d48988d116941735'; // Foursquare Nightlife
-  }
-  if(!radius) {
-    var radius = 500;  
-  }
+  var category = '4bf58dd8d48988d116941735'; // Foursquare Nightlife
+  var radius = 500;  
   var token = 'B2YI5GXCW022WC3F4FLP5SFHGGLG1LA0DCT2QSGQTXQVBYWV';
-  
   var url = 'https://api.foursquare.com/v2/venues/search'
       url += '?ll='+ll(location_ll);
       url += '&radius='+radius;
       url += '&categoryId='+category;
       url += '&intent=browse&oauth_token='+token+'&v=20130811';
-//      url = "https://api.foursquare.com/v2/venues/search?ll=52.529994200000004,13.408835700000001&radius=5000&categoryId=4bf58dd8d48988d1e0931735,4bf58dd8d48988d16d941735&intent=browse&oauth_token=B2YI5GXCW022WC3F4FLP5SFHGGLG1LA0DCT2QSGQTXQVBYWV&v=20130811"
-              //https://api.foursquare.com/v2/venues/search?ll=52.90875075801331,13.928215145934656&radius=500&categoryId=4bf58dd8d48988d116941735&intent=browse&oauth_token=B2YI5GXCW022WC3F4FLP5SFHGGLG1LA0DCT2QSGQTXQVBYWV&v=20130811
-  
-      console.log(url);
-  
+
   $.getJSON(url,function(data){
     venues = data.response.venues;
     if(venues.length > 0) {
-      
       venue = selectVenue(venues);
       
-      // TODO: This is a good point to insert some location chooser magic. Right now we just take the first result.
       venue_ll = [venue.location.lat,venue.location.lng];
       
-      // TODO: This updates the Map
       map.src = staticMapUrl(my_coordinates,friend_coordinates,320,320,venue_ll);
       $('.map').attr('src',staticMapUrl(my_coordinates,friend_coordinates,320,320,venue_ll));
       
       if(is_midpoint) {
-        // TODO: This updates the venue anme & distance field. Make this better.
-        $('.invite_venue_name').text(venue.name);
+        $('#mid_invite_venue').text(venue.name);
         $('.invite_venue_distance').text(getDistance(my_coordinates,venue_ll)+'km, '+getTravelTime(my_coordinates,venue_ll)+'min');
       }
-      
-      // TODO: This shows the closest places around you and your friend. Remove this later
       if(is_my_hood) {
-        $('.invite_venue_name').text(venue.name);
+        $('#my_invite_venue').text(venue.name);
         $('.invite_venue_distance').text(getDistance(my_coordinates,venue_ll)+'km, '+getTravelTime(my_coordinates,venue_ll)+'min');
       }
-      
       if(is_friend_hood) {
-        $('.invite_venue_name').text(venue.name);
+        $('#friend_invite_venue').text(venue.name);
         $('.invite_venue_distance').text(getDistance(my_coordinates,venue_ll)+'km, '+getTravelTime(my_coordinates,venue_ll)+'min');
       }
     } else {
