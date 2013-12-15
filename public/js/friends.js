fa71f87bbdbdd25167a36e7f9e299e81c3f0f60a
Johannes Ippen <jo@presskind.de>
2013-12-15 13:52:09 +0100

    Fixed User

--- public/js/friends.js
+++ public/js/friends.js
@@ -29,54 +29,17 @@ var loadFriends = function(user_id) {
   social.addNativeListener("getWWFriendsList", function(data)
   {
 	  for(i in data) {
-      data[i].location = {
-        "longitude": 52+Math.random()*0.1167+0.4166, // Min: .4166, Max: .5333
-        "latitude": 13+Math.random()*0.4+0.1666 // Min: .5666, Max: .1666
-      };
       friendlist.push(data[i]);
       addFriendtoList(data[i], i);
     }
     arrangeBubbles(document.querySelectorAll('#friendlist .friend'));
   });
   social.getWWFriendsList();
-  
-  /*$.getJSON(url,function(data){
-    for(i in data) {
-      data[i].location = {
-        "longitude": 52+Math.random()*0.1167+0.4166, // Min: .4166, Max: .5333
-        "latitude": 13+Math.random()*0.4+0.1666 // Min: .5666, Max: .1666
-      };
-      friendlist.push(data[i]);
-      addFriendtoList(data[i], i);
-    }
-    arrangeBubbles(document.querySelectorAll('#friendlist .friend'));
-  });*/
 };
-
-// loads an "amount" of random users and puts them into friend list
-var loadRandomFriends = function(amount) {
-  var seed = "012345678"; // loads different people
-  for(i = 0; i < amount; i++) {
-    $.getJSON('http://randomuser.me/g/?seed='+seed[i],function(data){
-      data = data.results[0];
-      data.user.location = {
-        "longitude": 52+Math.random()*0.1167+0.4166, // Min: .4166, Max: .5333
-        "latitude": 13+Math.random()*0.4+0.1666 // Min: .5666, Max: .1666
-      };
-      data.user.status = {
-        "description":"free"
-      };
-      friendlist.push(data.user);
-      addFriendtoList(data.user, i);
-      // TODO: put this in a better callback function
-      arrangeBubbles(document.getElementsByClassName('friend'));
-    });
-  };
-}
 
 // adds a person to the friend list
 var addFriendtoList = function(user, i) {
-    $('<li class="friend '+user.availability+'" onclick="createInviteFor('+user._id+')"></li>')
+    $('<li class="friend '+user.availability+'" onclick="createInviteFor(\''+i+'\')"></li>')
       .html('<span class="name">'+user.name+'</span>')
       .prepend('<img src="https://graph.facebook.com/'+user.fbID+'/picture?width=200&height=200">')
       .appendTo($('#friendlist'));
@@ -112,11 +75,10 @@ var showFriendProfile = function(user_id) {
 };
 
 var createInviteFor = function(user_id) {
+  alert(friendlist[user_id].name);
   friend_coordinates = [friendlist[user_id].location.longitude,friendlist[user_id].location.latitude];
   var map = document.getElementById('map');
       map.src = staticMapUrl(my_coordinates,friend_coordinates,300,180);
       
   getVenue(getMidpoint(my_coordinates,friend_coordinates),true);
-    
-
 }
