8341dc456e454a9755422aeaf3753bc2a785efab
Johannes Ippen <jo@presskind.de>
2013-12-15 19:57:06 +0100

    Frontend Tweaks

--- public/js/status.js
+++ public/js/status.js
@@ -1,6 +1,8 @@
 $('#status_free').live('mouseup touchend',function(){
   $('body').attr('data-status','free');
+  $('#status_busy').text('Set status to busy');
 });
 $('#status_busy').live('mouseup touchend',function(){
   $('body').attr('data-status','busy');
+  $('#status_free').text('Set status to free');
 });
