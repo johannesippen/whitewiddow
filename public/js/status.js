623dbb31b98614385fac1e5da656a8bab2d139cb
derPaul <dermediendesigner@gmail.com>
2013-12-29 17:49:23 +0100

    Abspeichern vom Status

--- public/js/status.js
+++ public/js/status.js
@@ -1,8 +1,12 @@
+//var statusSocial = new Social();
+
 $('#status_free').live('mouseup touchend',function(){
   $('body').attr('data-status','free');
   $('#status_busy').text('Set status to busy');
+  social.saveCurrentState("free");
 });
 $('#status_busy').live('mouseup touchend',function(){
   $('body').attr('data-status','busy');
   $('#status_free').text('Set status to free');
+  social.saveCurrentState("busy");
 });
